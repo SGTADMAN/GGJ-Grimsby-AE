f44a4daa75527df49c8fbf2ea4a200ef762ca0d7
Eduards Brown <eduard.brown123@hotmail.com>
2015-01-23 22:45:49 +0000

    Adding collision with imaginary object
    made player move on mouse click

---
+++ UnityFolder/Assets/Code/DetectCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		GameObject.Destroy (gameObject);
+		PlayerControl.clicked = false;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
