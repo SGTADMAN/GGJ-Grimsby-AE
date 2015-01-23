d5b7cf82a3188482522110cbb33c82622752f748
Eduards Brown <eduard.brown123@hotmail.com>
2015-01-23 20:41:18 +0000

    Player move
    Made placeholders, and a player moves when clicked on in it

---
+++ UnityFolder/Assets/Code/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float Speed;
+	private bool enter = false;
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnMouseDown()
+	{
+		transform.position = new Vector3 (transform.position.x + Speed, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
