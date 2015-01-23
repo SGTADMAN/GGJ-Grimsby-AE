cdc79c85b7af494879d5034bc9e40e9edf5d954a
Matster511 <footymatt2@hotmail.co.uk>
2015-01-23 20:53:12 +0000

    Camera Followed incorporated
    Added Camera Follow and allocated the player to the camera

---
+++ UnityFolder/Assets/Code/CameraFollow.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var cameraTarget : GameObject;
+var player : GameObject;
+var smoothTime : float = 0.1;
+var cameraFollowX : boolean = true;
+var cameraFollowY : boolean = true;
+var cameraFollowHeight : boolean = false;
+var cameraHeight : float = 2.5;
+var velocity : Vector2;
+private var thisTransform : Transform;
+
+function Start ()
+{
+thisTransform = transform;
+}
+
+function Update ()
+{
+ 
+if (cameraFollowX)
+{
+thisTransform.position.x = Mathf.SmoothDamp (thisTransform.position.x, cameraTarget.transform.position.x, velocity.x, smoothTime);
+}
+ 
+if (cameraFollowY)
+{
+thisTransform.position.y = Mathf.SmoothDamp (thisTransform.position.y, cameraTarget.transform.position.y, velocity.y, smoothTime);
+}
+ 
+if (!cameraFollowX && cameraFollowHeight)
+{
+camera.transform.position.y = cameraHeight;
+}
+ 
+}
