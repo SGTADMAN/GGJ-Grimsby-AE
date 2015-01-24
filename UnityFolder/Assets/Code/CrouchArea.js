7137b7c9ddbb71d4118f57d959deaf27cd167284
Matster511 <footymatt2@hotmail.co.uk>
2015-01-24 00:34:27 +0000

    Crouching box
    Adding an area in which the player crouches

---
+++ UnityFolder/Assets/Code/CrouchArea.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+function OnTriggerEnter2D(obj: Collider2D)
+{
+	if(obj.gameObject.name == "player") {
+		Debug.Log("Crouching");
+	}
+}
+
+function Update () {
+
+}
