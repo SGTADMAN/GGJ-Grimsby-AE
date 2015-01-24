de3aedaa675cb469b84775857a104c47447433ae
Matster511 <footymatt2@hotmail.co.uk>
2015-01-24 12:30:48 +0000

    Camera Follow and buttons
    Added camera follow to the main camera, added the player to target, made
    the buttons a child to the camera

---
+++ UnityFolder/Assets/Code/CameraFollow.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var target : GameObject;
+var yposition : float;
+
+function Start () {
+
+}
+
+function Update()
+{
+this.transform.position.y = target.transform.position.y;
+this.transform.position.x = target.transform.position.x;
+this.transform.position.y = yposition;
+} 
