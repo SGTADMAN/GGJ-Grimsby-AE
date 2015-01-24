9e8995316eedf929561fe3c1b168692d5d8f06a5
Eduards Brown <eduard.brown123@hotmail.com>
2015-01-24 16:12:59 +0000

    Updated the camera

--- UnityFolder/Assets/Code/CameraFollow.js
+++ UnityFolder/Assets/Code/CameraFollow.js
@@ -9,7 +9,7 @@ function Start () {
 
 function Update()
 {
-this.transform.position.y = target.transform.position.y;
-this.transform.position.x = target.transform.position.x;
-this.transform.position.y = yposition;
+	if(target.transform.position.y > 0)
+		this.transform.position.y = target.transform.position.y;
+	this.transform.position.x = target.transform.position.x;
 } 
