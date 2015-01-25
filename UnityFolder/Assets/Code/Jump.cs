68d42bf18cb51c0b7e46783b32796fa61005573b
Adam Lutton <adamlutton@adamluttonblog.co.uk>
2015-01-25 13:39:43 +0000

    blah

--- UnityFolder/Assets/Code/Jump.cs
+++ UnityFolder/Assets/Code/Jump.cs
@@ -2,17 +2,21 @@
 using System.Collections;
 
 public class Jump : MonoBehaviour {
-
+	bool mover = false;
 	Move m;
 	// Use this for initialization
 	void Start () {
 		m = GameObject.Find ("Player").GetComponent<Move> ();
 	}
-	void OnMouseDown()
-	{
-		m.Jump ();
-	}
+	//oid OnMouseDown()
+	//{
+	//	m.Jump ();
+	//}
 	// Update is called once per frame
 	void Update () {
+			if(Input.GetMouseButtonDown(0))
+			{
+				m.Jump();
+			}
 	}
 }
